2ad5f67c4a7e32b872e068533ddce401c10f1675
Surya Jayaraman <surya.j@fluxauto.xyz>
2022-04-30 11:25:36 +0530

    updated docstring for scripts

--- scripts/sort.py
+++ scripts/sort.py
@@ -18,6 +18,7 @@
 from __future__ import print_function
 
 import os
+from typing import Any
 import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
@@ -33,7 +34,32 @@ from filterpy.kalman import KalmanFilter
 np.random.seed(0)
 
 
-def linear_assignment(cost_matrix):
+def linear_assignment(cost_matrix) -> np.ndarray:
+  """Function solves the optimal assignment problem
+  using Jonker-Volgenant algorithm for dense (LAPJV)
+  matrices
+
+  Args:
+      cost_matrix (np.ndarray): (N,M) matrix where
+      c_ij refers to cost b/w ith row and jth column
+      elements
+
+  Returns:
+      np.ndarray : (P,2) where P = number of valid
+      assignments which are optimal based on input
+      cost matrix. eg : if cost matrix shape = (7,6),
+      there are 7 rows for which 6 columns are 
+      to be assigned.
+
+      Possible output = [[0,1]
+                         [1,2]
+                         [2,3]
+                         [3,4]
+                         [4,5]]
+      means there are 5 valid assignments, 2nd column
+      to 1st row, 3rd column to 2nd row and 4th column
+      to 3rd row and so on
+  """
   try:
     import lap
     _, x, y = lap.lapjv(cost_matrix, extend_cost=True)
@@ -44,100 +70,224 @@ def linear_assignment(cost_matrix):
     return np.array(list(zip(x, y)))
 
 
-def iou_batch(bb_test, bb_gt):
+
+def iou_batch(bb_test : np.ndarray, bb_gt : np.ndarray) -> np.ndarray:
+  """ Computes IOU between two bboxes given in [x1,y1,x2,y2] form 
+
+  Args:
+      bb_test (np.ndarray): (M,5) where M is number of bounding
+      boxes in bb_test. 5 columns indicate [x1,y1,x2,y2,confidence].
+
+      bb_gt (np.ndarray): (N,5) where N is number of bounding
+      boxes in bb_gt.
+
+  Returns:
+      iou (np.ndarray) : (M,N) array containing intersection
+      over union for each combination of bounding boxes in 
+      bb_test and bb_gt
+
+  Example
+    bb_test = (7,5) and bb_gt = (6,5), then return value
+    will have shape = (7,6)
   """
-  From SORT: Computes IOU between two bboxes in the form [x1,y1,x2,y2]
-  """
+  # assume bb_test shape = (7,5) and bb_gt shape = (6,5)
+  
+  # insert axis at 1st axis, bb_gt shape = (1,6,5)
   bb_gt = np.expand_dims(bb_gt, 0)
+
+  # insert axis at 2nd axis, bb_gt shape = (7,1,5)
   bb_test = np.expand_dims(bb_test, 1)
   
+  # xx1, yy1 denote the x,y coordinates of left top 
+  # corner of intersection between the bounding boxes
+  # xx1 shape = yy1 shape = (7,6)
   xx1 = np.maximum(bb_test[..., 0], bb_gt[..., 0])
   yy1 = np.maximum(bb_test[..., 1], bb_gt[..., 1])
+
+  # xx2, yy2 denote the x,y coordinates of right bottom 
+  # corner of intersection between the bounding boxes
+  # xx2 shape = yy2 shape = (7,6)
   xx2 = np.minimum(bb_test[..., 2], bb_gt[..., 2])
   yy2 = np.minimum(bb_test[..., 3], bb_gt[..., 3])
+  
+  # calculate the intersection area using width * height
+  # wh  = (7,6)
   w = np.maximum(0., xx2 - xx1)
   h = np.maximum(0., yy2 - yy1)
   wh = w * h
-  o = wh / ((bb_test[..., 2] - bb_test[..., 0]) * (bb_test[..., 3] - bb_test[..., 1])                                      
+
+  # iou = intersection / union where 
+  # union = area(bb_test) + are(bb_gt) - intersection
+  # iou shape = (7,6)
+  iou = wh / ((bb_test[..., 2] - bb_test[..., 0]) * (bb_test[..., 3] - bb_test[..., 1])                                      
     + (bb_gt[..., 2] - bb_gt[..., 0]) * (bb_gt[..., 3] - bb_gt[..., 1]) - wh)                                              
-  return(o)  
+  return(iou)  
 
 
-def convert_bbox_to_z(bbox):
+def convert_bbox_to_z(bbox : np.ndarray) -> np.ndarray:
+  """ Converts a bounding box in the form [x1,y1,x2,y2, confidence]
+   to measurement z in the form [x,y,s,r] where x,y is the centre of 
+   the box and s is the scale/area and r is the aspect ratio
+
+  Args:
+      bbox (np.ndarray): (5,) vector representing bounding box
+      top left and bottom right x,y coordinates and confidence
+
+  Returns:
+      np.ndarray : (4,1) measurement vector equivalent to input
+      bounding box
   """
-  Takes a bounding box in the form [x1,y1,x2,y2] and returns z in the form
-    [x,y,s,r] where x,y is the centre of the box and s is the scale/area and r is
-    the aspect ratio
-  """
-  w = bbox[2] - bbox[0]
-  h = bbox[3] - bbox[1]
-  x = bbox[0] + w/2.
-  y = bbox[1] + h/2.
-  s = w * h    #scale is just area
-  r = w / float(h)
+  # w,h,x,y are scalar doubles
+  w = bbox[2] - bbox[0]    # width of bbox
+  h = bbox[3] - bbox[1]    # height of bbox
+  x = bbox[0] + w/2.       # x-coordinate of centre of bbox
+  y = bbox[1] + h/2.       # y-coordinate of centre of bbox
+  s = w * h                # s = bbox area
+  r = w / float(h)         # w = width to height aspect ratio
   return np.array([x, y, s, r]).reshape((4, 1))
 
 
-def convert_x_to_bbox(x,score=None):
+
+def convert_x_to_bbox(x : np.ndarray, score : Any=None) -> np.ndarray:
+  """ Converts a bounding box in the centre form [x,y,s,r] to
+    [x1,y1,x2,y2] form where x1,y1 is the top left and 
+    x2,y2 is the bottom right coordinates
+
+  Args:
+      x (np.ndarray): (4,) vector representing bounding box
+      centre x,y coordinates and area and aspect ratio
+
+      score (Any, optional): confidence of bbox. Defaults to None.
+      If given, the value is given as such in output 
+
+  Returns:
+      np.ndarray: (1,N) vector representing bounding box
+      top left and bottom right x,y coordinates
+      N = 4 if score is None
+      N = 5 if score is not None
   """
-  Takes a bounding box in the centre form [x,y,s,r] and returns it in the form
-    [x1,y1,x2,y2] where x1,y1 is the top left and x2,y2 is the bottom right
-  """
+  # x[2] = (w*h), x[3] = (w/h)
+  # w and h are scalar doubles
   w = np.sqrt(x[2] * x[3])
   h = x[2] / w
+
+  # convert coordinates from centre to top left and bottom right
   if(score==None):
-    return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.]).reshape((1,4))
+    return np.array([x[0]-w/2., x[1]-h/2., 
+                     x[0]+w/2., x[1]+h/2.]).reshape((1,4))
   else:
-    return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.,score]).reshape((1,5))
+    return np.array([x[0]-w/2., x[1]-h/2.,
+                     x[0]+w/2., x[1]+h/2.,
+                     score]).reshape((1,5))
 
 
 class KalmanBoxTracker(object):
-  """
-  This class represents the internal state of individual tracked objects observed as bbox.
-  """
   count = 0
-  def __init__(self,bbox):
-    """
-    Initialises a tracker using initial bounding box.
-    """
-    #define constant velocity model
-    self.kf = KalmanFilter(dim_x=7, dim_z=4) 
-    self.kf.F = np.array([[1,0,0,0,1,0,0],[0,1,0,0,0,1,0],[0,0,1,0,0,0,1],[0,0,0,1,0,0,0],  [0,0,0,0,1,0,0],[0,0,0,0,0,1,0],[0,0,0,0,0,0,1]])
-    self.kf.H = np.array([[1,0,0,0,0,0,0],[0,1,0,0,0,0,0],[0,0,1,0,0,0,0],[0,0,0,1,0,0,0]])
+  def __init__(self,bbox : np.ndarray) -> None:
+    """ This class represents the internal state of individual
+    tracked objects observed as bbox.  Initialises a tracker 
+    using initial bounding box. State vector = 
+    [u,v,s,r,u_dot,v_dot,s_dot] as per SORT paper 
+    where u,v is centre of bbox, s = area of bbox, 
+    r = aspect ratio of bbox.
 
+    Motion model is a constant velocity model and measurement 
+    model assumes [u,v,s,r] states are observed
+
+    Args:
+        bbox (np.ndarray): (5,) vector representing bounding box
+        top left and bottom right x,y coordinates and confidence
+    """
+    # define constant velocity model
+    self.kf = KalmanFilter(dim_x=7, dim_z=4)
+
+    # transition matrix shape = (7,7)
+    self.kf.F = np.array([
+                          [1,0,0,0,1,0,0],
+                          [0,1,0,0,0,1,0],
+                          [0,0,1,0,0,0,1],
+                          [0,0,0,1,0,0,0],
+                          [0,0,0,0,1,0,0],
+                          [0,0,0,0,0,1,0],
+                          [0,0,0,0,0,0,1]
+                        ])
+
+    # measurement matrix shape = (4,7)
+    self.kf.H = np.array([
+                          [1,0,0,0,0,0,0],
+                          [0,1,0,0,0,0,0],
+                          [0,0,1,0,0,0,0],
+                          [0,0,0,1,0,0,0]
+                        ])
+
+    # measurement noise shape = (4,4)
     self.kf.R[2:,2:] *= 10.
-    self.kf.P[4:,4:] *= 1000. #give high uncertainty to the unobservable initial velocities
+
+    # state covariance shape = (7,7)
+    # give high uncertainty to the unobservable initial velocities
+    self.kf.P[4:,4:] *= 1000. 
     self.kf.P *= 10.
+
+    # process noise shape = (7,7)
     self.kf.Q[-1,-1] *= 0.01
     self.kf.Q[4:,4:] *= 0.01
 
+    # state is initialized to using input bbox 
     self.kf.x[:4] = convert_bbox_to_z(bbox)
-    self.time_since_update = 0
+
+    # counter for incrementing track number
     self.id = KalmanBoxTracker.count
     KalmanBoxTracker.count += 1
+
+    # track management variables
+    self.time_since_update = 0
     self.history = []
+
+    # hits = total number of detections associated with this track
+    # hit_streak = number of consecutive frames where track is associated
     self.hits = 0
     self.hit_streak = 0
     self.age = 0
 
-  def update(self,bbox):
+
+  def update(self,bbox : np.ndarray) -> None:
+    """ Performs Kalman filter update using observed bbox
+    measurement to get posterior state and covariance
+    Also resets tracker variables accordindly
+
+    Args:
+        bbox (np.ndarray): (5,) vector representing bounding box
+        top left and bottom right x,y coordinates and confidence
     """
-    Updates the state vector with observed bbox.
-    """
+    # reset history related varibles
     self.time_since_update = 0
     self.history = []
     self.hits += 1
     self.hit_streak += 1
+    
+    # convert bbox to [u,v,s,r] format and
+    # perform kalman filter update
     self.kf.update(convert_bbox_to_z(bbox))
 
-  def predict(self):
+
+
+  def predict(self) -> np.ndarray:
+    """ Predicts state using constant velocity model 
+    and returns predicted state
+
+    Returns:
+        np.ndarray: predicted state vector
     """
-    Advances the state vector and returns the predicted bounding box estimate.
-    """
+    # if the old area + in delta_area equals 0
+    # let delta_area be zero
     if((self.kf.x[6]+self.kf.x[2])<=0):
       self.kf.x[6] *= 0.0
+
+    # perform prediction
     self.kf.predict()
     self.age += 1
+
+    # if no state
     if(self.time_since_update>0):
       self.hit_streak = 0
     self.time_since_update += 1
